b230e3c87206dd10b7430c681a5d31239912f702
volonteMukovi <mukovivolonte@gmail.com>
2024-04-01 23:53:11 +0200

    retour a lexercices d'anniverssaire

--- ESS/essai.py
+++ ESS/essai.py
@@ -1,10 +1,10 @@
-# age=int(input("entre votre age"))
-# compte=0
-# for age in range(1,age):
-#     compte=compte+100
-#     compte=compte+2*age               
+age=int(input("entre votre age"))
+compte=0
+for age in range(1,age):
+    compte=compte+100
+    compte=compte+2*age               
 
-# print(compte)
+print(compte)
 # def moi():
 #     print("je suis vd")
 # def toi():
@@ -13,19 +13,19 @@
 # listfoction=[moi(),toi()]
 # for liste in listfoction:
 #     print(liste)
-class personne:
-    nom=""
-    age=0
-    def __init__(self,nom,age):
-        self.nom=nom
-        self.age=age
-    def decrir(self,nom,age):
-        self.nom=nom
-        self.age=age
-        print("vous vous appelle {} vous avez {} ans".format(nom,age))
+# class personne:
+#     nom=""
+#     age=0
+#     def __init__(self,nom,age):
+#         self.nom=nom
+#         self.age=age
+#     def decrir(self,nom,age):
+#         self.nom=nom
+#         self.age=age
+#         print("vous vous appelle {} vous avez {} ans".format(nom,age))
 
 
-names=input("entre votre nom")
-ages=int(input("entre votre age"))
-personne1 = personne("volonte",2)
-personne1.decrir(names,ages)
+# names=input("entre votre nom")
+# ages=int(input("entre votre age"))
+# personne1 = personne("volonte",2)
+# personne1.decrir(names,ages)
