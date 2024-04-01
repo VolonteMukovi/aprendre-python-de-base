bbdc124f81217362c8cc8205aeb9e094a920f5e2
volonteMukovi <mukovivolonte@gmail.com>
2024-04-01 23:17:50 +0200

    essai d'ajout de foction a l'intirieur de la liset

--- ESS/essai.py
+++ ESS/essai.py
@@ -1,7 +1,15 @@
-age=int(input("entre votre age"))
-compte=0
-for age in range(1,age):
-    compte=compte+100
-    compte=compte+2*age
+# age=int(input("entre votre age"))
+# compte=0
+# for age in range(1,age):
+#     compte=compte+100
+#     compte=compte+2*age
 
-print(compte)
+# print(compte)
+def moi():
+    print("je suis vd")
+def toi():
+    print("tues vd")
+
+listfoction=[moi(),toi()]
+for liste in listfoction:
+    print(liste)
