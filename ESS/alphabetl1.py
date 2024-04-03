7e0ce33719cc4753f0005c78a20107b3872a63d5
volonteMukovi <mukovivolonte@gmail.com>
2024-04-03 23:02:43 +0200

    commencement d'un nouvaeux projet enseignat

--- ESS/alphabetl1.py
+++ ESS/alphabetl1.py
@@ -1,16 +1,16 @@
 import os
 import shutil
-feilllessimple = ["calptuse","anana","levre"]
-feilllescompose = ["jus","pelouse","avoca"]
+# feilllessimple = ["calptuse","anana","levre"]
+# feilllescompose = ["jus","pelouse","avoca"]
 
-fell=input("entre la famiile de votre feillle")
-if(fell=="simple"):
-    for i in range(0,len(feilllessimple)):
-        print("")
-    print("\n", feilllessimple)
-elif(fell=="compose"):
-     for i in range(0,len(feilllescompose)):
-        print(i,feilllescompose)
+# fell=input("entre la famiile de votre feillle")
+# if(fell=="simple"):
+#     for i in range(0,len(feilllessimple)):
+#         print("")
+#     print("\n", feilllessimple)
+# elif(fell=="compose"):
+#      for i in range(0,len(feilllescompose)):
+#         print(i,feilllescompose)
 
 
 
@@ -18,27 +18,25 @@ elif(fell=="compose"):
 # Chemin du dossier où vous souhaitez organiser les fichiers
 dossier_cible = "D:\z"
 
-# Créez les dossiers de A à Z
-# lettres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-# for lettre in lettres:
-#     chemin_dossier = os.path.join(dossier_cible, lettre)
-#     os.makedirs(chemin_dossier, exist_ok=True)
-#     print(lettre,lettres)
+#Créez les dossiers de A à Z
+lettres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for lettre in lettres:
+    chemin_dossier = os.path.join(dossier_cible, lettre)
+    os.makedirs(chemin_dossier, exist_ok=True)
+    print(lettre,lettres)# Choisissez le dossier source contenant vos fichiers
+dossier_source = "E:\English musics"
 
-# # Choisissez le dossier source contenant vos fichiers
-# dossier_source = "E:\English musics"
-
-# # Parcourez les fichiers dans le dossier source
-# for nom_fichier in os.listdir(dossier_source):
-#     chemin_fichier_source = os.path.join(dossier_source, nom_fichier)
-#     if os.path.isfile(chemin_fichier_source):
-#         # Obtenez la première lettre du nom de fichier
-#         premiere_lettre = nom_fichier[0].upper()
-#         # Vérifiez si le dossier de destination existe
-#         if premiere_lettre in lettres:
-#             chemin_dossier_destination = os.path.join(dossier_cible, premiere_lettre)
-#             # Déplacez le fichier vers le dossier de destination
-#             shutil.move(chemin_fichier_source, chemin_dossier_destination)
-#             print(f"Le fichier '{nom_fichier}' a été déplacé vers '{chemin_dossier_destination}'.")
+# Parcourez les fichiers dans le dossier source
+for nom_fichier in os.listdir(dossier_source):
+    chemin_fichier_source = os.path.join(dossier_source, nom_fichier)
+    if os.path.isfile(chemin_fichier_source):
+        # Obtenez la première lettre du nom de fichier
+        premiere_lettre = nom_fichier[0].upper()
+        # Vérifiez si le dossier de destination existe
+        if premiere_lettre in lettres:
+            chemin_dossier_destination = os.path.join(dossier_cible, premiere_lettre)
+            # Déplacez le fichier vers le dossier de destination
+            shutil.move(chemin_fichier_source, chemin_dossier_destination)
+            print(f"Le fichier '{nom_fichier}' a été déplacé vers '{chemin_dossier_destination}'.")
 
 print("Organisation terminée !")
